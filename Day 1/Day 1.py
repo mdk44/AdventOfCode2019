976f2af7458e3a66494a3a492ff813aefc98cbfe
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-01 11:20:21 -0600

    Part 2 working

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -3,9 +3,19 @@ text_file = open(input_file)
 lines = text_file.read().split()
 
 # Part 1
-sum = 0
+sum_p1 = 0
 for line in lines:
     cur_wt = int(int(line) / 3) - 2
-    sum += cur_wt
+    sum_p1 += cur_wt
 
-print sum
+print "Sum for Part 1: " + str(sum_p1)
+
+sum_p2 = 0
+for line in lines:
+    cur_wt = int(line)
+    while cur_wt > 0:
+        cur_wt = int(cur_wt / 3) - 2
+        if cur_wt > 0:
+            sum_p2 += cur_wt
+
+print "Sum for Part 2: " + str(sum_p2)
