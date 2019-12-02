30e14d07d8f97632b47f569298c2d1245af98ffe
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-02 06:26:07 -0600

    Updates

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -2,13 +2,15 @@
 # 1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
 # 2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
 # 2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
-# 1,1,1,4,99,5,6,0,99
+# 1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99
 
-input_file = 'Day 2\\Input.csv'
-text_file = open(input_file)
-lines = text_file.read()
+# input_file = 'Day 2\\Input.csv'
+# text_file = open(input_file)
+# lines = text_file.read().split(',')
 
 input_text = '1,0,0,0,99'
-input_text = '2,3,0,3,99'
-input_text = '2,4,4,5,99,0'
-input_text = '1,1,1,4,99,5,6,0,99'
+# input_text = '2,3,0,3,99'
+# input_text = '2,4,4,5,99,0'
+# input_text = '1,1,1,4,99,5,6,0,99'
+lines = [int(line) for line in input_text.split(',')]
+print lines
