63620364e7f797f86b6846c329341063d7d84e7c
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-04 11:47:34 -0600

    Part 2 final

--- Day 4/Day 4.py
+++ Day 4/Day 4.py
@@ -16,3 +16,30 @@ for num in range(start_num, end_num + 1):
 
 print "Part 1: " + str(pass_count) # Correct!
 
+pass_count = 0
+for num in range(start_num, end_num + 1):
+    flag = 0
+    dig1 = int(str(num)[0:1])
+    dig2 = int(str(num)[1:2])
+    dig3 = int(str(num)[2:3])
+    dig4 = int(str(num)[3:4])
+    dig5 = int(str(num)[4:5])
+    dig6 = int(str(num)[5:6])
+    if dig1 <= dig2 and dig2 <= dig3 and dig3 <= dig4 and dig4 <= dig5 and dig5 <= dig6:
+        if dig1 == dig2 and dig2 != dig3:
+            flag = 1
+        elif dig1 != dig2 and dig2 == dig3 and dig3 != dig4:
+            flag = 1
+        elif dig2 != dig3 and dig3 == dig4 and dig4 != dig5:
+            flag = 1
+        elif dig3 != dig4 and dig4 == dig5 and dig5 != dig6:
+            flag = 1
+        elif dig4 != dig5 and dig5 == dig6:
+            flag = 1
+        else:
+            flag = 0
+        
+    if flag == 1:
+        pass_count += 1
+
+print "Part 2: " + str(pass_count) # Correct!
