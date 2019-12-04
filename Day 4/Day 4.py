bd7c1f4dbff519f15ca438b12e82e45576aec661
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-04 07:22:13 -0600

    Part 1 Final

--- Day 4/Day 4.py
+++ Day 4/Day 4.py
@@ -1,22 +1,7 @@
-# key facts about the password:
-
-
-# Two adjacent digits are the same (like 22 in 122345).
-# Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-# Other than the range rule, the following are true:
-
-# 111111 meets these criteria (double 11, never decreases).
-# 223450 does not meet these criteria (decreasing pair of digits 50).
-# 123789 does not meet these criteria (no double).
-# How many different passwords within the range given in your puzzle input meet these criteria?
-
-# Your puzzle input is 108457-562041.
-
 start_num = 108457
 end_num = 562041
 num = start_num
 pass_count = 0
-test = 112345
 
 for num in range(start_num, end_num + 1):
     dig1 = int(str(num)[0:1])
@@ -25,3 +10,9 @@ for num in range(start_num, end_num + 1):
     dig4 = int(str(num)[3:4])
     dig5 = int(str(num)[4:5])
     dig6 = int(str(num)[5:6])
+    if dig1 <= dig2 and dig2 <= dig3 and dig3 <= dig4 and dig4 <= dig5 and dig5 <= dig6:
+        if dig1 == dig2 or dig2 == dig3 or dig3 == dig4 or dig4 == dig5 or dig5 == dig6:
+            pass_count += 1
+
+print "Part 1: " + str(pass_count) # Correct!
+
