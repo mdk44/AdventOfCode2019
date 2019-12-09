59335441912699a4e1df2a6e20aac12484ea4baf
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-09 09:18:37 -0600

    Day 3 part 1 works

--- Day 3/Day 3.py
+++ Day 3/Day 3.py
@@ -3,12 +3,23 @@ import sys
 import time
 from PIL import Image, ImageDraw
 
+input_file1 = 'Day 3\\Wire1.csv'
+text_file1 = open(input_file1)
+input_file2 = 'Day 3\\Wire2.csv'
+text_file2 = open(input_file2)
+wire1 = text_file1.read().split(',')
+wire2 = text_file2.read().split(',')
+output_file = 'Day 3\\Wires.png'
+
 CORNER = 0
 NONE = 1
 UP = 2
 ACROSS = 3
 INTERSECT = 4
 CENTER = 5
+CORNER2 = 6
+UP2 = 7
+ACROSS2 = 8
 
 grid_rep = {
     CORNER: "+",
@@ -16,8 +27,45 @@ grid_rep = {
     UP: "|",
     ACROSS: "-",
     INTERSECT: "X",
-    CENTER: "o"
+    CENTER: "o",
+    CORNER2: "s",
+    UP2: "l",
+    ACROSS2: "="
 }
+
+def image_grid(grid):
+    corner = (226, 25, 224)
+    none = (0, 0, 0)
+    up = (226, 25, 224)
+    across = (226, 25, 224)
+    intersect = (255, 255, 0)
+    center = (175, 175, 175)
+    corner2 = (175, 175, 242)
+    up2 = (175, 175, 242)
+    across2 = (175, 175, 242)
+    width = 18000
+    height = 12000
+    img = Image.new('RGB', (width, height), color = none)
+    dr = ImageDraw.Draw(img)
+    for y in grid:
+        for x in grid[y]:
+            if grid[y][x] == CORNER:
+                dr.rectangle(((x,y),(x,y)),fill=corner)
+            elif grid[y][x] == UP:
+                dr.rectangle(((x,y),(x,y)),fill=up)
+            elif grid[y][x] == ACROSS:
+                dr.rectangle(((x,y),(x,y)),fill=across)
+            elif grid[y][x] == INTERSECT:
+                dr.rectangle(((x,y),(x,y)),fill=intersect)
+            elif grid[y][x] == CENTER:
+                dr.rectangle(((x,y),(x,y)),fill=center)
+            elif grid[y][x] == CORNER2:
+                dr.rectangle(((x,y),(x,y)),fill=corner2)
+            elif grid[y][x] == UP2:
+                dr.rectangle(((x,y),(x,y)),fill=up2)
+            elif grid[y][x] == ACROSS2:
+                dr.rectangle(((x,y),(x,y)),fill=across2)
+    img.save(output_file)
 
 def print_grid(grid):
     for y in sorted(grid.iterkeys()):
@@ -25,13 +73,6 @@ def print_grid(grid):
         for x in sorted(grid[y].iterkeys()):
             grid_line += grid_rep[grid[y][x]]
         print grid_line
-
-input_file1 = 'Day 3\\Wire1.csv'
-text_file1 = open(input_file1)
-input_file2 = 'Day 3\\Wire2.csv'
-text_file2 = open(input_file2)
-wire1 = text_file1.read().split(',')
-wire2 = text_file2.read().split(',')
 
 # test1 = ['R8','U5','L5','D3']
 # test2 = ['U7','R6','D4','L4']
@@ -44,19 +85,17 @@ wire1_dir = []
 wire1_num = []
 wire2_dir = []
 wire2_num = []
-for i in test1:
+for i in wire1:
     wire1_dir.append(i[0])
     wire1_num.append(int(i[1:]))
-for j in test2:
+for j in wire2:
     wire2_dir.append(j[0])
     wire2_num.append(int(j[1:]))
 
-grid_size_x = 500
-grid_size_y = 500
-grid_cent_x = 250
-grid_cent_y = 250
-
-#crap
+grid_size_x = 17020
+grid_size_y = 11900
+grid_cent_x = 4265
+grid_cent_y = 6206
 
 grid = {}
 for y in range(0,grid_size_y):
@@ -113,8 +152,8 @@ for i in range(0, len(wire2_dir)):
                 ans_x.append(cur_x)
                 ans_y.append(y)
             else:
-                grid[y][cur_x] = UP
-        grid[new_y][cur_x] = CORNER
+                grid[y][cur_x] = UP2
+        grid[new_y][cur_x] = CORNER2
         cur_y = new_y
     if wire2_dir[i] == "D":
         new_y = cur_y + wire2_num[i]
@@ -124,8 +163,8 @@ for i in range(0, len(wire2_dir)):
                 ans_x.append(cur_x)
                 ans_y.append(y)
             else:
-                grid[y][cur_x] = UP
-        grid[new_y][cur_x] = CORNER
+                grid[y][cur_x] = UP2
+        grid[new_y][cur_x] = CORNER2
         cur_y = new_y
     if wire2_dir[i] == "R":
         new_x = cur_x + wire2_num[i]
@@ -135,8 +174,8 @@ for i in range(0, len(wire2_dir)):
                 ans_x.append(x)
                 ans_y.append(cur_y)
             else:
-                grid[cur_y][x] = ACROSS
-        grid[cur_y][new_x] = CORNER
+                grid[cur_y][x] = ACROSS2
+        grid[cur_y][new_x] = CORNER2
         cur_x = new_x
     if wire2_dir[i] == "L":
         new_x = cur_x - wire2_num[i]
@@ -146,15 +185,15 @@ for i in range(0, len(wire2_dir)):
                 ans_x.append(x)
                 ans_y.append(cur_y)
             else:
-                grid[cur_y][x] = ACROSS
-        grid[cur_y][new_x] = CORNER
+                grid[cur_y][x] = ACROSS2
+        grid[cur_y][new_x] = CORNER2
         cur_x = new_x
 
-# print_grid(grid)
+image_grid(grid)
 # print ans_x
 # print ans_y
 
 man_dist = []
 for a in range(0, len(ans_x)):
     man_dist.append( abs(ans_x[a] - grid_cent_x) + abs(ans_y[a] - grid_cent_y))
-print "Part 1: " + str(min(man_dist))
+print "Part 1: " + str(min(man_dist))  # Correct but holy crap lol
