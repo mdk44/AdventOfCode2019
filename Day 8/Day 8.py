c61514cf91864f09b7a4988b7975ab120a322aa9
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-09 06:29:45 -0600

    Updates to Day 8

--- Day 8/Day 8.py
+++ Day 8/Day 8.py
@@ -9,9 +9,22 @@ lines = [int(line) for line in text_file.read()]
 # 100 - 124
 
 start_num = 0
-count_zero = 0
+count_0 = 0
+count_1 = 0
+count_2 = 0
+num_0 = 20000
 num_1 = 0
 num_2 = 0
 
 for i in range(start_num, start_num + 125):
-    print i
+    if lines[i] == 0:
+        count_0 += 1
+    elif lines[i] == 1:
+        count_1 += 1
+    elif lines[i] == 2:
+        count_2 += 1
+if count_0 < num_0:
+    num_0 = count_0
+    num_1 = count_1
+    num_2 = count_2
+    print "0: " + str(num_0) + "  1x2: " + str(num_1 * num_2)
