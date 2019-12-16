ac2fe146065c0d8856588c7f2e1b4c3a953eb950
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-15 21:27:26 -0600

    Unsuccessful day 5 tinkering

--- Day 5/Day 5.py
+++ Day 5/Day 5.py
@@ -4,10 +4,19 @@
 
 lines = [3,0,4,0,99]
 
+init_input = 1
 new_lines = []
 for line in lines:
     new_lines.append(line)
 start_num = 0
+
+input1 = new_lines[start_num]
+# will revisit
+
+
+
+
+
 while new_lines[start_num] != 99:
     if new_lines[start_num] == 1:
         input1 = new_lines[start_num + 1]
@@ -22,7 +31,7 @@ while new_lines[start_num] != 99:
         new_lines[output_pos] = new_lines[input1] * new_lines[input2]
         start_num += 4
     elif new_lines[start_num] == 3:
-        input1 = 1 # illustrative example to show that I can do this
+        input1 = init_input
         output_pos = new_lines[start_num + 1]
         new_lines[output_pos] = input1
         start_num += 2
@@ -34,5 +43,7 @@ print new_lines
 
 test1 = 1002
 test2 = str(test1)
-print test2[-2:]
-print test2[-4]
+testlen = len(test2)
+print int(test2[-2:])
+print test2[-testlen]
+print testlen
