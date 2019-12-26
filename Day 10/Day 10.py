d9262227495e9b0236338d7e6f411f45300afd60
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-26 14:18:43 -0600

    Day 10 part 1 correct

--- Day 10/Day 10.py
+++ Day 10/Day 10.py
@@ -1,23 +1,62 @@
-input_file = 'Day 10\\Test1.csv'
+import math
+
+# input_file = 'Day 10\\Test1.csv'
 # input_file = 'Day 10\\Test2.csv'
 # input_file = 'Day 10\\Test3.csv'
 # input_file = 'Day 10\\Test4.csv'
 # input_file = 'Day 10\\Test5.csv'
-# input_file = 'Day 10\\Input.csv'
+input_file = 'Day 10\\Input.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
-def print_grid(grid):
-    for y in sorted(grid.iterkeys()):
-        grid_line = ""
-        for x in sorted(grid[y].iterkeys()):
-            grid_line += grid[y][x]
-        print grid_line
+# NEED TO FIGURE OUT TYPE HINTS
 
-grid = {}
-for y in range(0,len(lines)):
-    grid[y] = {}
-    for x in range(0,len(lines[0])):
-        grid[y][x] = lines[y][x]
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-print_grid(grid)
+    def __str__(self):
+        return "x: " + str(self.x) + ", y: " + str(self.y)
+    
+    def __eq__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return self.x == other.x and self.y == other.y
+
+    def get_angle(self, origin):
+        angle = math.atan2(self.y - origin.y, self.x - origin.x) * 180 / math.pi
+        if angle < 0:
+            return angle + 360
+        else:
+            return angle
+
+def make_points(lines):
+    points = []
+    for y in range(0,len(lines)):
+        line = lines[y]
+        for x in range(0,len(line)):
+            if line[x] == "#":
+                point = Point(x, y)
+                points.append(point)
+    return points
+
+def get_angles(points, origin):
+    angles = dict()
+    for point in points:
+        if point == origin:
+            continue
+        else:
+            angles[origin.get_angle(point)] = True
+    return len(angles)
+
+def get_highest(lines):
+    maxm = 0
+    points = make_points(lines)
+    for point in points:
+        count = get_angles(points, point)
+        if count > maxm:
+            maxm = count
+    return maxm
+
+print("Part 1: " + str(get_highest(lines))) # Correct!
